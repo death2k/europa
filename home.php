5a5045e9cdb5f3d917ad7d6e353acda575bb7df7
System Administrator <root@MACBOOK-PRO.local>
2018-03-29 14:27:46 -0300

    commit inicial do projeto

---
+++ home.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Standard Meta -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+<script src="semantic.min.js"></script>
+
+  <!-- Site Properties -->
+  <title>Sticky Example - Semantic</title>
+
+  <link rel="stylesheet" type="text/css" href="components/reset.css">
+  <link rel="stylesheet" type="text/css" href="components/site.css">
+
+  <link rel="stylesheet" type="text/css" href="components/container.css">
+  <link rel="stylesheet" type="text/css" href="components/grid.css">
+  <link rel="stylesheet" type="text/css" href="components/header.css">
+  <link rel="stylesheet" type="text/css" href="components/image.css">
+  <link rel="stylesheet" type="text/css" href="components/menu.css">
+
+  <link rel="stylesheet" type="text/css" href="components/divider.css">
+  <link rel="stylesheet" type="text/css" href="components/list.css">
+  <link rel="stylesheet" type="text/css" href="components/segment.css">
+  <link rel="stylesheet" type="text/css" href="omponents/dropdown.css">
+  <link rel="stylesheet" type="text/css" href="components/icon.css">
+  <link rel="stylesheet" type="text/css" href="components/transition.css">
+
+  <script src="components/jquery.min.js"></script>
+  <script src="components/transition.js"></script>
+  <script src="components/dropdown.js"></script>
+  <script src="components/visibility.js"></script>
+
+<style>
+
+</style>
+
+</head>
+<body>
+
+<div class="ui attached stackable menu">
+  <div class="ui container">
+    <a class="item">
+      <i class="home icon"></i> Home
+    </a>
+    <a class="item">
+      <i class="grid layout icon"></i> Browse
+    </a>
+    <a class="item">
+      <i class="mail icon"></i> Messages
+    </a>
+    <div class="ui simple dropdown item">
+      More
+      <i class="dropdown icon"></i>
+      <div class="dropdown menu">
+        <a class="item"><i class="edit icon"></i> Edit Profile</a>
+        <a class="item"><i class="globe icon"></i> Choose Language</a>
+        <a class="item"><i class="settings icon"></i> Account Settings</a>
+      </div>
+    </div>
+    <div class="right item">
+      <div class="ui input"><input type="text" placeholder="Search..."></div>
+    </div>
+  </div>
+</div>
+
+<div class="ui green inverted menu">
+  <a class="active item">
+    <i class="home icon"></i> Home
+  </a>
+  <a class="item">
+    <i class="mail icon"></i> Messages
+  </a>
+  <a class="item">
+    <i class="user icon"></i> Friends
+  </a>
+</div>
+<div class="ui large menu">
+  <a class="active item">
+    <i class="home icon"></i> Home
+  </a>
+  <a class="item">
+    <i class="mail icon"></i> Messages
+  </a>
+  <div class="right menu">
+    <div class="item">
+      <div class="ui icon input">
+        <input type="text" placeholder="Search...">
+        <i class="search link icon"></i>
+      </div>
+    </div>
+    <div class="ui dropdown item">
+      Language <i class="dropdown icon"></i>
+    </div>
+    <div class="item">
+        <div class="ui teal button">Sign Up</div>
+    </div>
+  </div>
+</div>
+  
+
+  <div class="ui equal width center aligned padded grid">
+  <div class="row">
+    <div class="olive column">
+    <div class="ui four column doubling stackable grid container">
+  <div class="column">
+    <p>Flavio</p>
+    <p>Ribeiro</p>
+  </div>
+  <div class="column">
+    <p>Isabella</p>
+    <p>Ribeiro</p>
+  </div>
+  <div class="column">
+    <p>Lara</p>
+    <p>Ribeiro</p>
+  </div>
+  <div class="column">
+    <p>Heitor&Bruno</p>
+    <p>Ribeiro</p>
+  </div>
+</div>
+    </div>
+    <div class="black column">
+      Black
+    </div>
+  </div>
+  <div class="row" style="background-color: #869D05;color: #FFFFFF;">
+    <div class="column">Custom Row</div>
+  </div>
+  <div class="row">
+    <div class="black column">
+      Black
+    </div>
+    <div class="olive column">
+    <div class="ui fluid selection dropdown">
+  <input type="hidden" name="user">
+  <i class="dropdown icon"></i>
+  <div class="default text">Select Friend</div>
+  <div class="menu">
+    <div class="item" data-value="jenny">
+      <img class="ui mini avatar image" src="/images/avatar/small/jenny.jpg">
+      Jenny Hess
+    </div>
+    <div class="item" data-value="elliot">
+      <img class="ui mini avatar image" src="/images/avatar/small/elliot.jpg">
+      Elliot Fu
+    </div>
+    <div class="item" data-value="stevie">
+      <img class="ui mini avatar image" src="/images/avatar/small/stevie.jpg">
+      Stevie Feliciano
+    </div>
+    <div class="item" data-value="christian">
+      <img class="ui mini avatar image" src="/images/avatar/small/christian.jpg">
+      Christian
+    </div>
+    <div class="item" data-value="matt">
+      <img class="ui mini avatar image" src="/images/avatar/small/matt.jpg">
+      Matt
+    </div>
+    <div class="item" data-value="justen">
+      <img class="ui mini avatar image" src="/images/avatar/small/justen.jpg">
+      Justen Kitsune
+    </div>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+   
+
+</body>
+
+</html>
+
